43705dde3aa07e7a72193936ee2eb3c0c9302672
Hiếu Ngô Quang <lanlanabc288@gmail.com>
2022-05-16 15:17:36 +0700

    ok

--- app/Http/Controllers/importController.php
+++ app/Http/Controllers/importController.php
@@ -8,8 +8,14 @@ use Illuminate\Http\Request;
 class importController extends Controller
 {
     public function storange(Request $request){
-        $image = $request->file('image');
-        $path = Storage::disk('public')->put('image_test', $image);
+        // $image = $request->file('image');
+        // $path = Storage::disk('public')->put('image_test', $image);
+
+        $images = $request->file('images');
+        foreach($images as $image){
+            $path = Storage::disk('public')->put('image_test', $image);
+        }
+
         $path = Storage::disk('public')->files('image_test');
         $list = [];
         foreach($path as $file){
